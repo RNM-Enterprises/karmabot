f654c360c3c61a8d239e177525f8cb630529104c
Joey Harrison <joeyh021@icloud.com>
2022-07-19 22:41:22 +0100

    first rough karmastore

--- karmabot/karma/karma_store.py
+++ karmabot/karma/karma_store.py
@@ -1,12 +1,54 @@
-class KarmaStore(dict):
-    def load() -> None:
-        """
-        load data using pickle
-        """
-        pass
+import pickle as pkl
+from aiofile import async_open
+from discord.ext import tasks
+from os import path
+from collections.abc import MutableMapping
 
-    def save() -> None:
+
+class KarmaStore(MutableMapping[int, int]):
+    __store: dict[int, int]
+
+    def __init__(self, filename: str, interval: int = 1):
+        self.filename = filename
+
+        # load internal state if file already exists
+        if path.exists("filename"):
+            self.load()
+        # if not, can just start from empty
+
+        self.bgsave.change_interval(minutes=interval)
+        self.bgsave.start()
+
+    async def save(self):
         """
-        save data using pickle
+        Save internal store to file
         """
-        pass
+        raw_data = pkl.dumps(self.__store)
+        async with async_open(self.filename, "wb") as f:
+            await f.write(raw_data)
+
+    def load(self):
+        """
+        Replace current internal store with file contents
+        """
+        with open(self.filename, "rb") as f:
+            raw_data: bytes = f.read()
+        loaded_data = pkl.loads(raw_data)
+
+        # we could have loaded literally fucking anything
+        # validate it
+        if isinstance(loaded_data, dict) and all(
+            isinstance(k, int) and isinstance(v, int) for (k, v) in loaded_data.values()
+        ):
+            self.__store = loaded_data
+        else:
+            raise TypeError(
+                f"Object loaded from {self.filename} is not valid karma data"
+            )
+
+    @tasks.loop()
+    async def bgsave(self):
+        """
+        Background loop to periodically save file to disk
+        """
+        await self.save()
