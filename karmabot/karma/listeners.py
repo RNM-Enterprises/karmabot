3c2ffc43994365093bc1fceafac8eaadcaa36120
Joey Harrison <joeyh021@icloud.com>
2022-07-20 20:51:12 +0100

    first reaction listeners half done

--- karmabot/karma/listeners.py
+++ karmabot/karma/listeners.py
@@ -1,9 +1,43 @@
-import discord.ext.commands as commands
-
+from discord.ext import commands
+import discord
 from .karma_store import KarmaStore
+
+# need to use raw reaction events
+# see docs (https://discordpy.readthedocs.io/en/latest/api.html#discord.on_raw_reaction_add)
 
 
 class Listeners(commands.Cog):
     def __init__(self, bot: commands.Bot, karma_store: KarmaStore):
-        self.bot = bot
-        self.karma_store = karma_store
+        self.__bot = bot
+        self.__karma_store = karma_store
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, event: discord.RawReactionActionEvent):
+        message = await self.get_message(event)
+
+        self.__karma_store[message.author.id] += 1
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, event: discord.RawReactionActionEvent):
+        message = await self.get_message(event)
+        self.__karma_store[message.author.id] -= 1
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_clear(self, event: discord.RawReactionActionEvent):
+        pass
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_emoji(self, event: discord.RawReactionActionEvent):
+        pass
+
+    # helper to get message from raw reaction event
+    async def get_message(
+        self,
+        event: discord.RawReactionActionEvent
+        | discord.RawReactionClearEvent
+        | discord.RawReactionClearEmojiEvent,
+    ) -> discord.Message:
+        channel = self.__bot.get_channel(event.channel_id)
+        assert isinstance(channel, discord.abc.Messageable)
+        message = await channel.fetch_message(event.message_id)
+        return message
