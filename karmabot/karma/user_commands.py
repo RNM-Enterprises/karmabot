c7e7b4e975953d763474253c59beb497879ecedb
Oliver Tansley <olivertansley@btinternet.com>
2022-07-22 18:33:32 +0100

    profile pictures now circular

--- karmabot/karma/user_commands.py
+++ karmabot/karma/user_commands.py
@@ -1,6 +1,7 @@
-import cmath
+
 import discord
 import discord.ext.commands as commands
+
 import numpy as np
 import requests
 from .karma_store import KarmaStore
@@ -18,41 +19,40 @@ class UserCommands(commands.Cog):
         """
 
         # open main image and drawer
-        response = Image.open('./Resources/Template.jpg')
+        response = Image.new('RGB',(934,282))
         response_drawer = ImageDraw.Draw(response)
         
         # fill main image with colored background
-        response.paste( (19,21,20), (0,0,response.size[0],response.size[1]))
+        response.paste( (0,0,0), (0,0,response.size[0],response.size[1]))
 
         # write users name and discriminator        
         myFont = ImageFont.truetype(font='arial.ttf',size=54)
-        response_drawer.text((274,104), str(ctx.author),font=myFont,fill=(255,255,255))
+        response_drawer.text((320,104), str(ctx.author),font=myFont,fill=(255,255,255))
         
         # write users karma
         myFont = ImageFont.truetype(font='arial.ttf',size=64)
-        response_drawer.text((274,response.size[1] -(64+44)), f'karma: {self.karma_store.get(int(ctx.author.id))}',font=myFont,fill=(233,79,55))
+        response_drawer.text((320,response.size[1] -(64+44)), f'karma: {self.karma_store.get(int(ctx.author.id))}',font=myFont,fill=(233,79,55))
         
         # collect users avatar
-        avatar = Image.open(requests.get(str(ctx.author.avatar),stream=True).raw)
+        avatar = Image.open(requests.get(str(ctx.author.avatar),stream=True).raw).convert('RGB').resize((254,254))
         img_arr = np.array(avatar)
 
         # crop user avatar into circle
-        lum_img = Image.new('L',(avatar.size[0],avatar.size[1]), 0)
+        lum_img = Image.new('L',(avatar.size[0],avatar.size[1]),0)
         ImageDraw.Draw(lum_img).pieslice(((0,0),(lum_img.size[0],lum_img.size[1])),0,360,fill=255, outline='white')
-        lum_img_arr = np.array(lum_img)        
+        lum_img_arr = np.array(lum_img)
+      
+
+        framed_avatar_arr = np.dstack((img_arr , lum_img_arr))
         
-        framed_avatar_arr = np.dstack((img_arr, lum_img_arr))
+        framed_avatar = Image.fromarray(framed_avatar_arr)
 
-        print(framed_avatar_arr[128][128])
-        
-        framed_avatar = Image.fromarray(framed_avatar_arr, 'RGB')
+        response.paste(framed_avatar,(14,14),mask=framed_avatar)
 
-        response.paste(framed_avatar,(0,0))
-
-        response.save('./Resources/Template.jpg')
+        response.save('./Resources/Template.png')
         response.close()
 
-        await ctx.reply(file=discord.File('./Resources/Template.jpg'))
+        await ctx.reply(file=discord.File('./Resources/Template.png'))
 
     @commands.command()
     async def leaderboard(self,ctx:commands.Context):
