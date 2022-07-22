fca1ae142eeadc2958d5f62f7fb81b6e30bd7973
Oliver Tansley <olivertansley@btinternet.com>
2022-07-22 16:07:48 +0100

    began making circular avatar

--- karmabot/karma/user_commands.py
+++ karmabot/karma/user_commands.py
@@ -1,7 +1,8 @@
-from urllib import response
+import cmath
 import discord
 import discord.ext.commands as commands
-
+import numpy as np
+import requests
 from .karma_store import KarmaStore
 from PIL import Image, ImageDraw, ImageFont
 
@@ -13,29 +14,45 @@ class UserCommands(commands.Cog):
     @commands.command()
     async def karma(self, ctx: commands.Context):
         """
-        Tells you your fake karma (because real karma doesnt exist yet)
+        Tells you how much karma you have
         """
-        # self.karma_store[int(ctx.author.id)] += 1
-        # await ctx.reply(
-        #     f"{ctx.author}'s karma isnt {self.karma_store.get(int(ctx.author.id))}"
-        # )
 
+        # open main image and drawer
         response = Image.open('./Resources/Template.jpg')
-    
-        drawer = ImageDraw.Draw(response)
-        myFont = ImageFont.truetype(font='arial.ttf',size=65)
-        drawer.text((4,4), ctx.author.name,font=myFont,fill=(255,255,255))
-        drawer.text((4,69), f'karma:{self.karma_store.get(int(ctx.author.id))}',font=myFont,fill=(255,255,255))
+        response_drawer = ImageDraw.Draw(response)
+        
+        # fill main image with colored background
+        response.paste( (19,21,20), (0,0,response.size[0],response.size[1]))
 
+        # write users name and discriminator        
+        myFont = ImageFont.truetype(font='arial.ttf',size=54)
+        response_drawer.text((274,104), str(ctx.author),font=myFont,fill=(255,255,255))
+        
+        # write users karma
+        myFont = ImageFont.truetype(font='arial.ttf',size=64)
+        response_drawer.text((274,response.size[1] -(64+44)), f'karma: {self.karma_store.get(int(ctx.author.id))}',font=myFont,fill=(233,79,55))
+        
+        # collect users avatar
+        avatar = Image.open(requests.get(str(ctx.author.avatar),stream=True).raw)
+        img_arr = np.array(avatar)
+
+        # crop user avatar into circle
+        lum_img = Image.new('L',(avatar.size[0],avatar.size[1]), 0)
+        ImageDraw.Draw(lum_img).pieslice(((0,0),(lum_img.size[0],lum_img.size[1])),0,360,fill=255, outline='white')
+        lum_img_arr = np.array(lum_img)        
+        
+        framed_avatar_arr = np.dstack((img_arr, lum_img_arr))
+
+        print(framed_avatar_arr[128][128])
+        
+        framed_avatar = Image.fromarray(framed_avatar_arr, 'RGB')
+
+        response.paste(framed_avatar,(0,0))
 
         response.save('./Resources/Template.jpg')
+        response.close()
 
         await ctx.reply(file=discord.File('./Resources/Template.jpg'))
-
-        response.paste( (0,0,0), (0,0,response.size[0],response.size[1]))
-        response.save('./Resources/Template.jpg')
-
-        response.close()
 
     @commands.command()
     async def leaderboard(self,ctx:commands.Context):
