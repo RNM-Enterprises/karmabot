292590c4b6bf5d5ff1635e750303094529f8fc35
Ollie <oliver.lawrence169@gmail.com>
2022-07-22 19:48:07 +0100

    Added cog reloading, admin commands

--- karmabot/karma/admin_commands.py
+++ karmabot/karma/admin_commands.py
@@ -7,3 +7,21 @@ class AdminCommands(commands.Cog):
     def __init__(self, bot: commands.Bot, karma_store: KarmaStore):
         self.bot = bot
         self.karma_store = karma_store
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx: commands.Context, module: str):
+        """Reloads a module."""
+        try:
+            await self.bot.unload_extension(module)
+            await self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.reply("\N{PISTOL}")
+            await ctx.reply("{}: {}".format(type(e).__name__, e))
+        else:
+            await ctx.reply("\N{OK HAND SIGN}")
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def cool(self, ctx: commands.Context):
+        await ctx.reply("Yeah youre cool asf")
