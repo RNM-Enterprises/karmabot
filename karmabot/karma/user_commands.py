afd5f606a982e86c6536a77f30978fdd1f7f4a9e
Oliver Tansley <olivertansley@btinternet.com>
2022-07-22 21:58:40 +0100

    improving karma command

--- karmabot/karma/user_commands.py
+++ karmabot/karma/user_commands.py
@@ -1,58 +1,26 @@
-
+from typing import Optional
 import discord
 import discord.ext.commands as commands
-
-import numpy as np
-import requests
 from .karma_store import KarmaStore
-from PIL import Image, ImageDraw, ImageFont
-
+from .karma_cards import get_karma_card
+from io import BytesIO
 class UserCommands(commands.Cog):
     def __init__(self, bot: commands.Bot, karma_store: KarmaStore):
         self.bot = bot
         self.karma_store = karma_store
 
     @commands.command()
-    async def karma(self, ctx: commands.Context):
+    async def karma(self, ctx: commands.Context,member:Optional[discord.Member] | str):
         """
-        Tells you how much karma you have
+        Tells you how much karma a user has
         """
-
-        # open main image and drawer
-        response = Image.new('RGB',(934,282))
-        response_drawer = ImageDraw.Draw(response)
-        
-        # fill main image with colored background
-        response.paste( (0,0,0), (0,0,response.size[0],response.size[1]))
-
-        # write users name and discriminator        
-        myFont = ImageFont.truetype(font='arial.ttf',size=54)
-        response_drawer.text((320,104), str(ctx.author),font=myFont,fill=(255,255,255))
-        
-        # write users karma
-        myFont = ImageFont.truetype(font='arial.ttf',size=64)
-        response_drawer.text((320,response.size[1] -(64+44)), f'karma: {self.karma_store.get(int(ctx.author.id))}',font=myFont,fill=(233,79,55))
-        
-        # collect users avatar
-        avatar = Image.open(requests.get(str(ctx.author.avatar),stream=True).raw).convert('RGB').resize((254,254))
-        img_arr = np.array(avatar)
-
-        # crop user avatar into circle
-        lum_img = Image.new('L',(avatar.size[0],avatar.size[1]),0)
-        ImageDraw.Draw(lum_img).pieslice(((0,0),(lum_img.size[0],lum_img.size[1])),0,360,fill=255, outline='white')
-        lum_img_arr = np.array(lum_img)
-      
-
-        framed_avatar_arr = np.dstack((img_arr , lum_img_arr))
-        
-        framed_avatar = Image.fromarray(framed_avatar_arr)
-
-        response.paste(framed_avatar,(14,14),mask=framed_avatar)
-
-        response.save('./Resources/Template.png')
-        response.close()
-
-        await ctx.reply(file=discord.File('./Resources/Template.png'))
+        if member is None:
+            await ctx.reply(file=discord.File(await get_karma_card(ctx.author,self.karma_store[ctx.author.id])))
+        elif isinstance(member,discord.Member):
+            #get_karma_card(member,self.karma_store[member.id])
+            await ctx.reply(file=discord.File('./Resources/Template.png'))
+        else:
+           await ctx.reply(f"{member} isnt in this server?")
 
     @commands.command()
     async def leaderboard(self,ctx:commands.Context):
