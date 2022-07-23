87e57e578c0057a886579dd2b41836aaf6a58260
Joey Harrison <joeyh021@icloud.com>
2022-07-23 13:12:33 +0100

    fix image sending

--- karmabot/karma/user_commands.py
+++ karmabot/karma/user_commands.py
@@ -4,35 +4,33 @@ import discord
 import discord.ext.commands as commands
 from .karma_store import KarmaStore
 from .karma_cards import get_karma_card
-from io import BytesIO
-from PIL import Image
+
+
 class UserCommands(commands.Cog):
     def __init__(self, bot: commands.Bot, karma_store: KarmaStore):
         self.bot = bot
         self.karma_store = karma_store
 
     @commands.command()
-    async def karma(self, ctx: commands.Context,member:Optional[discord.Member] | str):
+    async def karma(
+        self, ctx: commands.Context, member: Optional[discord.Member] | str
+    ):
         """
         Tells you how much karma a user has
         """
         if member is None:
-            picture = await get_karma_card(ctx.author,self.karma_store[ctx.author.id])
+            await ctx.reply(
+                file=await get_karma_card(ctx.author, self.karma_store[ctx.author.id])
+            )
 
-            pic_bytes = BytesIO()
-            picture.show()
-            picture.save(pic_bytes,format="PNG")
-            with open("./resources/template.PNG","wb") as f:
-                f.write(pic_bytes.getbuffer())
-            await ctx.reply(file=discord.File("./resources/template.PNG"))
-        elif isinstance(member,discord.Member):
-            picture = await get_karma_card(ctx.author,self.karma_store[member.id])
-            await ctx.reply(file=discord.File("./resources/template.PNG"))
+        elif isinstance(member, str):
+            await ctx.reply(f"User {member} does not exist in this server?")
+
         else:
-           await ctx.reply(f"{member} isnt in this server?")
-        
-    @commands.command()
-    async def leaderboard(self,ctx:commands.Context):
-        pass
+            await ctx.reply(
+                file=await get_karma_card(member, self.karma_store[member.id])
+            )
 
-    
+    @commands.command()
+    async def leaderboard(self, ctx: commands.Context):
+        pass
