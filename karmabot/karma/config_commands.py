febc1ae39a12f05fbefd9d2141311b27bbca0b54
Joey Harrison <joeyh021@icloud.com>
2022-07-24 13:29:07 +0100

    config commands done

--- karmabot/karma/config_commands.py
+++ karmabot/karma/config_commands.py
@@ -1,31 +1,68 @@
 import discord.ext.commands as commands
 import discord
 from karmabot import KarmaBot
+from .emoji import UnicodeEmoji, emoji_value
+from functools import partial
 
 
 class ConfigCommands(commands.Cog):
     def __init__(self, bot: KarmaBot):
-        self.bot = bot
+        self.__bot = bot
+        self.__emoji_value = partial(emoji_value, self.__bot.config.emoji)
 
-    @commands.group()
-    async def emoji(self, ctx: commands.Context):
-        pass
+    @commands.group(invoke_without_command=True)
+    async def emoji(self, ctx: commands.Context, emoji: discord.Emoji | UnicodeEmoji):
+        value = self.__emoji_value(emoji)
+
+        await ctx.reply(
+            f"Emoji {str(emoji)} has value {value if value is not None else 0}"
+        )
 
     @emoji.command()
-    async def get(self, ctx: commands.Context):
+    async def list(self, ctx: commands.Context):
         """
-        Gets the karma values for all emoji, or a specific emoji
+        Gets the karma values for all emoji
         """
-        await ctx.reply(f"{self.bot.config.emoji}")
+        await ctx.reply(str(self.__bot.config.emoji))
 
     @emoji.command()
-    async def set(self, ctx: commands.Context, emoji: discord.PartialEmoji, value: int):
-        if emoji.is_custom_emoji():
-            pass
+    async def set(
+        self, ctx: commands.Context, emoji: discord.Emoji | UnicodeEmoji, value: int
+    ):
+        if isinstance(emoji, discord.Emoji):
+            self.__bot.config.emoji[emoji.name] = value
+        elif isinstance(emoji, UnicodeEmoji):
+            self.__bot.config.emoji[str(emoji)] = value
+        else:  # unreachable
+            assert False
 
-        elif emoji.is_unicode_emoji():
-            pass
+        self.__bot.config.save()
+        await ctx.reply(f"Emoji {str(emoji)} has been given a karma value of {value}")
 
     @emoji.command()
-    async def unset(self, ctx: commands.Context, emoji: discord.PartialEmoji):
-        pass
+    async def unset(self, ctx: commands.Context, emoji: discord.Emoji | UnicodeEmoji):
+        async def try_del(key: str | int) -> bool:
+            try:
+                del self.__bot.config.emoji[key]
+                return True
+            except KeyError:
+                await ctx.reply(f"Emoji {str(emoji)} is already worth 0 karma!")
+                return False
+
+        if isinstance(emoji, discord.Emoji):
+            if emoji.id in self.__bot.config.emoji.keys():
+                if not await try_del(emoji.id):
+                    return
+            elif emoji.name in self.__bot.config.emoji.keys():
+                if not await try_del(emoji.name):
+                    return
+
+        elif isinstance(emoji, UnicodeEmoji):
+            if not await try_del(str(emoji)):
+                return
+
+        else:  # unreachable
+            assert False
+
+        self.__bot.config.save()
+        await ctx.reply(f"Emoji {str(emoji)} has been given a karma value of 0")
